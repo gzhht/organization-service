53612bff0c0aa611bdac547df804e7832dd40118
zjx-immersion <jianxin_home@163.com>
2022-01-12 00:16:16 +0800

    [jason] support for local launch

--- src/main/java/pl/piomin/services/organization/client/DepartmentClient.java
+++ src/main/java/pl/piomin/services/organization/client/DepartmentClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import pl.piomin.services.organization.model.Department;
 
-@FeignClient(name = "department")
+@FeignClient(name = "${feign.department.name}", url = "${feign.department.url}")
 public interface DepartmentClient {
 
 	@GetMapping("/organization/{organizationId}")
